f304a719f6f9044adaaee7f0a16cf80643c218b5
exmoure <exmoure@gmail.com>
2014-12-26 15:58:07 -0700

    SurfaceCreator WIP.

--- Assets/scripts/SurfaceCreator.cs
+++ Assets/scripts/SurfaceCreator.cs
@@ -10,6 +10,8 @@ public class SurfaceCreator : MonoBehaviour
 	private Mesh mesh;
 
 	public float frequency = 1f;
+
+	public Vector3 rotation = Vector3.zero;
 	
 	[Range(1, 8)]
 	public int octaves = 1;
@@ -22,10 +24,15 @@ public class SurfaceCreator : MonoBehaviour
 	
 	[Range(1, 3)]
 	public int dimensions = 3;
+
+	public float maxHeight = 1f;
 	
 	public NoiseMethodType type;
 	
 	public Gradient coloring;
+
+	private Color[] colors;
+	private Vector3[] vertices;
 
 	[Range(1,200)]
 	public int resolution = 0;
@@ -47,6 +54,7 @@ public class SurfaceCreator : MonoBehaviour
 		{
 			CreateGrid();
 		}
+		MakeNoise();
 	}
 
 	void CreateGrid()
@@ -57,23 +65,20 @@ public class SurfaceCreator : MonoBehaviour
 		// Create vertices, normals, uv, and color values.
 		int n = resolution + 1;
 		int vertLength = n*n;
-		Vector3[] vertices = new Vector3[vertLength];
+		this.vertices = new Vector3[vertLength];
 		Vector3[] normals = new Vector3[vertLength];
 		Vector2[] uv = new Vector2[vertLength]; 
-		Color[] colors = new Color[vertLength];
+		colors = new Color[vertLength];
 		float stepSize = 1f / resolution;
-		for(int v = 0, y = 0; y <= resolution; y++)
-		{
-			for(int x = 0; x <= resolution; x++, v++)
-			{
-				vertices[v] = new Vector3(x * stepSize - 0.5f, y * stepSize - 0.5f);
-				normals[v] = Vector3.back;
-				uv[v] = new Vector2(x * stepSize, y * stepSize);
-				float r =  ((float)x)/resolution;
-				float b =  ((float)y)/resolution;
-				colors[v] = new Color(r,0,b,1);
+		for (int v = 0, z = 0; z <= resolution; z++) {
+			for (int x = 0; x <= resolution; x++, v++) {
+				vertices[v] = new Vector3(x * stepSize - 0.5f, 0f, z * stepSize - 0.5f);
+				colors[v] = Color.black;
+				normals[v] = Vector3.up;
+				uv[v] = new Vector2(x * stepSize, z * stepSize);
 			}
 		}
+
 		mesh.vertices = vertices;
 		mesh.normals = normals;
 		mesh.uv = uv;
@@ -94,6 +99,38 @@ public class SurfaceCreator : MonoBehaviour
 
 			}
 		}
+
+		mesh.vertices = vertices;
 		mesh.triangles = triangles;
+	}
+
+	void MakeNoise()
+	{
+		Quaternion q = Quaternion.Euler(rotation);
+		Vector3 point00 = q * transform.TransformPoint(new Vector3(-0.5f,-0.5f));
+		Vector3 point10 = q * transform.TransformPoint(new Vector3( 0.5f,-0.5f));
+		Vector3 point01 = q * transform.TransformPoint(new Vector3(-0.5f, 0.5f));
+		Vector3 point11 = q * transform.TransformPoint(new Vector3( 0.5f, 0.5f));
+		
+		NoiseMethod method = Noise.noiseMethods[(int)type][dimensions - 1];
+		float stepSize = 1f / resolution;
+		for (int v = 0, y = 0; y <= resolution; y++) {
+			Vector3 point0 = Vector3.Lerp(point00, point01, y * stepSize);
+			Vector3 point1 = Vector3.Lerp(point10, point11, y * stepSize);
+			for (int x = 0; x <= resolution; x++, v++) {
+				Vector3 point = Vector3.Lerp(point0, point1, x * stepSize);
+				float sample = Noise.Sum(method, point, frequency, octaves, lacunarity, persistence);
+			    sample = type == NoiseMethodType.Value ? (sample - 0.5f) : (sample * 0.5f);	
+				if (type != NoiseMethodType.Value) {
+					sample = sample * 0.5f + 0.5f;
+				}
+				colors[v] = coloring.Evaluate(sample);
+				vertices[v].y = Mathf.Lerp(0,maxHeight,sample);
+			}
+		}
+
+		mesh.vertices = vertices;
+		mesh.colors = colors;
+		mesh.RecalculateNormals();
 	}
 }
--- Assets/shaders/SurfaceShader.shader
+++ Assets/shaders/SurfaceShader.shader
@@ -7,7 +7,7 @@
 		LOD 200
 		
 		CGPROGRAM
-		#pragma surface surf NoLighting
+		#pragma surface surf Lambert
 
 		sampler2D _MainTex;
 
@@ -15,14 +15,6 @@
 			float2 uv_MainTex;
 			half4 color : COLOR;
 		};
-
-		fixed4 LightingNoLighting(SurfaceOutput IN, fixed3 lightDir, fixed atten)
-		{
-			fixed4 c;
-			c.rgb = IN.Albedo;
-			c.a = IN.Alpha;
-			return c;
-		}
 
 		void surf (Input IN, inout SurfaceOutput o) {
 			half4 c = tex2D (_MainTex, IN.uv_MainTex);
