db3bec64aeb71a9628b5882c6b0da48447ce8345
exmoure <exmoure@gmail.com>
2014-12-28 15:42:27 -0700

    Column WIP.

--- Assets/scripts/Column.cs
+++ Assets/scripts/Column.cs
@@ -1,10 +1,13 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
+using System.Collections.Generic;
 
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class Column : MonoBehaviour
 {
 	int cachedSides = -1;
+
 
 	void Update()
 	{
@@ -20,6 +23,7 @@ public class Column : MonoBehaviour
 		CreateMesh();
 	}
 
+
 	void CreateMesh()
 	{
 		var filter = GetComponent<MeshFilter>();
@@ -28,46 +32,65 @@ public class Column : MonoBehaviour
 		filter.mesh = mesh;
 		mesh.Clear();
 
-        Vector3[] vertices = new Vector3[sides+1];
-        vertices[0] = transform.position;
+		var ring1 = createPoly(transform.position + Vector3.back, sides, radius * 0.5f);
+		var ring2 = createPoly(transform.position, sides, radius);
 
-		Vector3[] outerVertices = ShapeFactory.UnitRegular(transform.position, sides, radius);	
-		for(int i = 1; i < vertices.Length; i++)
-		{
-			vertices[i] = outerVertices[i-1];
-		}
+        MeshStruct data = new MeshStruct();
+        data.vertices = new Vector3[ring1.vertices.Length + ring2.vertices.Length];
+        for(int i = 0; i < sides; i++)
+        {
+        	data.vertices[i] = ring1.vertices[i];
+        }
 
-		Vector2[] uvs = new Vector2[vertices.Length];
-		Vector3[] normals = new Vector3[vertices.Length];
+        for(int i = sides; i < sides*2; i++)
+        {
+        	data.vertices[i] = ring2.vertices[i-sides];
+        }
 
-		for(int i = 0; i < vertices.Length; i++)
-		{
-			var v = vertices[i];
-			uvs[i] = new Vector2(v.x, v.y);
-			normals[i] = Vector3.back;
-		}
+        data.uv = new Vector2[data.vertices.Length];
 
-		int[] triangles = new int[sides*3];
+        int[] triangles = new int[sides * 2 * 3];
 
-		// Fill all but final triangle.
-		int tri = 0;
-		for(int t = 0; t < triangles.Length - 3; t+=3)
-		{
-			triangles[t] = 0;
-			triangles[t+1] = tri+2;
-			triangles[t+2] = tri+1;
-			tri++;
-		}
+        for(int t = 0, v = 0; v < sides-1 /* x ringCount-1 */; t+=6, v++)
+        {
+        	triangles[t] = v;
+        	triangles[t+1] = v+sides+1;
+        	triangles[t+2] = v+sides;
+        	triangles[t+3] = v;
+        	triangles[t+4] = v+1;
+        	triangles[t+5] = v+sides+1;
+        }
 
-		// Final triangle.
-		triangles[triangles.Length - 1] = 0;
-		triangles[triangles.Length - 2] = sides;
-		triangles[triangles.Length - 3] = 1;
+        // Final two triangles
+        triangles[triangles.Length-1] = sides - 1;
+        triangles[triangles.Length-2] = (sides * 2)-1;
+        triangles[triangles.Length-3] = sides;
+        triangles[triangles.Length-4] = sides - 1;
+        triangles[triangles.Length-5] = sides;
+        triangles[triangles.Length-6] = 0;
 
-		mesh.vertices = vertices;
-		mesh.uv = uvs;
+        mesh.vertices = data.vertices;
+		mesh.uv = data.uv;
 		mesh.triangles = triangles;
 		mesh.RecalculateNormals();
+		mesh.RecalculateBounds();
+	}
+
+	MeshStruct createPoly(Vector3 centerPoint, int sides, float r)
+	{
+		MeshStruct result = new MeshStruct();
+		result.vertices = ShapeFactory.UnitRegular(centerPoint, sides, r);	
+		result.uv = new Vector2[result.vertices.Length];
+		result.normals = new Vector3[result.vertices.Length];
+
+		for(int i = 0; i < result.vertices.Length; i++)
+		{
+			var v = result.vertices[i];
+			result.uv[i] = new Vector2(v.x, v.y);
+			result.normals[i] = Vector3.back;
+		}
+
+		return result;
 	}
 
 
@@ -75,18 +98,5 @@ public class Column : MonoBehaviour
 	public int sides = 8;
 
 	public float radius = 1f;
-
-	/*void OnDrawGizmos()
-	{
-		var vertices = Polygon.UnitRegular(transform.position, sides,radius);	
-		Gizmos.color = Color.black;
-
-		for(int i = 0; i < vertices.Length; i++)
-		{
-			Vector3 from = i == 0 ? vertices[vertices.Length-1] : vertices[i-1];
-			Vector3 to = vertices[i];
-			Gizmos.DrawLine(from,to);
-		}
-	}*/
 }
 
---
+++ Assets/scripts/MeshStruct.cs
@@ -0,0 +1,10 @@
+using System;
+using UnityEngine;
+
+public struct MeshStruct
+{
+	public Vector3[] vertices;
+	public Vector3[] normals;
+	public Vector2[] uv;
+	public int[] triangles;
+}
